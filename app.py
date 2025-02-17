3f80324ab5fec1b9bc531ee8c94c506fc4d402fd
nonhlanhlaCindi <nontethelelocindi42@gmail.com>
2025-02-17 08:42:09 +0200

    Final changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  
+app.secret_key = 'quiz'  
 
 questions = [
     # General Questions
